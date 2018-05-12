afb3e722e7d2726aeb2c7c03d6f2cd70a6d5c784
caoyong <caoyong@shopexunit.com>
2018-05-12 20:38:08 +0800

    将暂存区的代码添加到GitHub仓库中管理

---
+++ repositoryProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  repositoryProject
+//
+//  Created by caoyong on 2018/5/12.
+//  Copyright © 2018年 caoyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
