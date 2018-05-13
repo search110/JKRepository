4ec7cbea8689dfd8b8184e44af8282f4f90e2b19
caoyong <caoyong@shopexunit.com>
2018-05-13 13:50:33 +0800

    添加第二分支model

---
+++ repositoryProject/Sencond/sencondModel.m
@@ -0,0 +1,13 @@
+//
+//  sencondModel.m
+//  repositoryProject
+//
+//  Created by caoyong on 2018/5/13.
+//  Copyright © 2018年 caoyong. All rights reserved.
+//
+
+#import "sencondModel.h"
+
+@implementation sencondModel
+
+@end
